80ebd487b26cf5f42fff81f2dcf67bdcd3fecddb
KOBAYASHI Shigeru <shigeru.kb@gmail.com>
2015-12-12 15:52:20 +0900

    Modify to refer to original resouce

--- userscript/nicopedia-flmmlonhtml5.user.js
+++ userscript/nicopedia-flmmlonhtml5.user.js
@@ -1,16 +1,14 @@
 // ==UserScript==
 // @name        Nicopedia-FlMMLonHTML5
 // @namespace   https://github.com/kosh04/FlMMLonHTML5
-// @version     0.20150804
+// @version     0.20151212
 // @description ニコニコ大百科のピコカキコプレーヤーをFlMMLonHTML5に置き換える (デバッグ用)
 // @updateURL   https://github.com/kosh04/FlMMLonHTML5/raw/feature-userscript/userscript/nicopedia-flmmlonhtml5.user.js
 // @grant       GM_getResourceText
 // @match       http://dic.nicovideo.jp/*
-// @resource    flmmlworker.js  https://rawgit.com/kosh04/FlMMLonHTML5/feature-userscript/project/flmmlworker-raw.js
+// @resource    flmmlworker.js  https://rawgit.com/carborane3/FlMMLonHTML5/master/project/flmmlworker-raw.js
 // @author      kosh (mono)
 // ==/UserScript==
-
-"use strict";
 
 function loadScript(src) {
     var s = document.createElement("script");
@@ -19,8 +17,8 @@ function loadScript(src) {
     document.head.appendChild(s);
 }
 
-loadScript("https://rawgit.com/kosh04/FlMMLonHTML5/feature-userscript/project/flmmlonhtml5-raw.js");
-loadScript("https://rawgit.com/kosh04/FlMMLonHTML5/feature-userscript/project/flmmlplayer-raw.js");
+loadScript("https://rawgit.com/carborane3/FlMMLonHTML5/master/project/flmmlonhtml5-raw.js");
+loadScript("https://rawgit.com/carborane3/FlMMLonHTML5/master/project/flmmlplayer-raw.js");
 
 var code = GM_getResourceText("flmmlworker.js");
 var blob = new Blob([code], { type: "text/javascript" });
@@ -31,7 +29,7 @@ var pikoList = document.querySelectorAll("[id^=piko]");
 [].forEach.call(pikoList, function(piko) {
     if (!piko.id.match(/^piko\d+/)) return;
     var mml_id = piko.id.substring(4); // "piko777" -> "777"
-    var code = "new FlMMLPlayer(%s).show(this);".replace("%s", JSON.stringify({
+    var code = "(function(elem) { var player = new FlMMLPlayer(%s); elem.parentNode.replaceChild(player.svg, elem); })(this)".replace("%s", JSON.stringify({
       mmlURL: "/mml/" + mml_id,
       height: "1.65em",
       underground: true,
